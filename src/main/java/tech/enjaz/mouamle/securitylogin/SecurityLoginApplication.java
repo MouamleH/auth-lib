aeb5fa3a5bbc754deb8599a2790346ecd8bdf93d
Mouamle <Mouamle.hasan@gmail.com>
2019-06-03 15:05:08 +0300

    Project setup

---
+++ src/main/java/tech/enjaz/mouamle/securitylogin/SecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package tech.enjaz.mouamle.securitylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityLoginApplication.class, args);
+    }
+
+}
